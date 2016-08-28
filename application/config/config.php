a07be8eec38d0bc180893c450d0aca0da11f7dbb
poncho10101 <vgcwebmaster@gmail.com>
2016-08-28 02:24:25 -0600

    solicitudes admin, agregado el becar solicitante

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://127.0.0.1/becados';
+$config['base_url'] = 'http://10.236.243.230/becados';
 
 /*
 |--------------------------------------------------------------------------
